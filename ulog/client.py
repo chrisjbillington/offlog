39e86aff579656f56c4769cb86a239bbd69a2f60
chrisjbillington <chrisjbillington@gmail.com>
2023-07-01 16:27:21 +1000

    Queue unsent data
    Retry sending it whenever we're sending data, and attempt to flush it
    all at close.

--- ulog/client.py
+++ ulog/client.py
@@ -3,6 +3,7 @@ import builtins
 import io
 import socket
 import select
+from collections import deque
 
 from . import DEFAULT_TIMEOUT, DEFAULT_SOCK_PATH
 
@@ -23,6 +24,57 @@ def _make_exception(response):
     return exc_class(message)
 
 
+class _ByteQueue:
+    """Class to queue unsent data. Stores data in a dequeue of BytesIO objects
+    containing up to BUFSIZE bytes each. All interior elements have exactly BUFSIZE
+    bytes each, but the first and last elements may have fewer. Idea is to call put() to
+    queue unsent data, to call peek() to get a chunk of data to be sent from the queue
+    without removing, and then to report how many bytes were successfully sent by
+    calling done(n), which clears the last n bytes from the queue. len() returns the
+    length of the queue in bytes."""
+
+    def __init__(self):
+        self.q = deque()
+
+    def __len__(self):
+        qlen = len(self.q)
+        if not qlen:
+            return 0
+        n = self.q[0].getbuffer().nbytes
+        if qlen > 1:
+            n += self.q[-1].getbuffer().nbytes
+        if qlen > 2:
+            n += BUFSIZE * (qlen - 2)
+        return n
+
+    def put(self, data):
+        """Append data to the back of the queue"""
+        if self:  # non-empty
+            curr_chunk_nbytes_avail = BUFSIZE - self.q[-1].getbuffer().nbytes
+            if curr_chunk_nbytes_avail:
+                self.q[-1].write(data[:curr_chunk_nbytes_avail])
+                data = data[curr_chunk_nbytes_avail:]
+        while data:
+            chunk = io.BytesIO()
+            chunk.write(data[:BUFSIZE])
+            self.q.append(chunk)
+            data = data[BUFSIZE:]
+
+    def peek(self):
+        """Get up to BUFSIZE bytes from the front of the queue without removing them"""
+        return self.q[0].getvalue()
+
+    def done(self, n):
+        """Remove nbytes from the front of the queue"""
+        if not n:
+            return
+        chunk = self.q.popleft()
+        if n < chunk.getbuffer().nbytes:
+            newchunk = io.BytesIO()
+            newchunk.write(chunk.getvalue()[n:])
+            self.q.appendleft(newchunk)
+
+
 class ProxyFile:
     """Object to proxy appending file writes via a running ulog server"""
 
@@ -39,27 +91,17 @@ class ProxyFile:
         self.poller = select.epoll()
         self.poller.register(self.sock, select.EPOLLIN)
         self._recv_buf = io.BytesIO()
+        self._sendqueue = _ByteQueue()
         self._connect()
         self._open(filepath)
 
-    def _send(self, data, return_unsent=True):
-        # Send as much as we can without blocking. If would block, either return unsent
-        # data if `return_unsent` or raise BlockingIOError. If BrokenPipeError, check if
-        # the server sent us an error and raise it if so.
-        while data:
-            try:
-                sent = self.sock.send(data)
-                data = data[sent:]
-            except BlockingIOError:
-                if return_unsent:
-                    return data
-                raise
-            except BrokenPipeError:
-                # Check if the server responded with an error saying why it booted us:
-                response = self._recv_msg(timeout=0)
-                if response:
-                    raise _make_exception(response) from None
-                raise
+    def _connect(self):
+        try:
+            self.sock.connect(self.sock_path)
+        except FileNotFoundError:
+            self.sock.close()
+            emsg = f"server socket {self.sock_path} not found"
+            raise FileNotFoundError(emsg) from None
 
     def _recv_msg(self, timeout=None):
         # recv until null byte. If timeout or EOF, raise but leave data read so far in
@@ -67,47 +109,107 @@ class ProxyFile:
         if timeout is None:
             timeout = self.timeout
         while self.poller.poll(timeout):
-            data = self.sock.recv(BUFSIZE)
+            try:
+                data = self.sock.recv(BUFSIZE)
+            except ConnectionResetError:
+                self.sock.close()
+                raise
             msg, null, extradata = data.partition(b'\0')
             self._recv_buf.write(msg)
             if not data:
+                self.sock.close()
                 raise EOFError
             if null:
                 msg = self._recv_buf.getvalue()
                 self._recv_buf = io.BytesIO()
                 self._recv_buf.write(extradata)
                 return msg
+        self.sock.close()
         raise TimeoutError("No response from server")
-
-    def _connect(self):
-        try:
-            self.sock.connect(self.sock_path)
-        except FileNotFoundError:
-            emsg = f"server socket {self.sock_path} not found"
-            raise FileNotFoundError(emsg) from None
 
     def _open(self, filepath):
         filepath = os.fsencode(filepath)
         if b'\0' in filepath:
+            self.sock.close()
             raise ValueError("embedded null byte in filepath")
-        self._send(os.path.abspath(filepath) + b'\0', return_unsent=False)
-        response = self._recv_msg(self.timeout)
+        self.write(os.path.abspath(filepath) + b'\0')
+        response = self._recv_msg()
         if response != FILE_OK:
+            self.sock.close()
             raise _make_exception(response)
 
-    def write(self, msg):
-        data = msg.encode('utf8')
-        unsent = self._send(data, return_unsent=True)
-        if unsent:
-            # TODO: make config to optionally block, drop, or buffer?
-            raise BlockingIOError
+    def _checksend(self, data):
+        """Send data and return number of bytes sent. On BrokenPipeError, check if the
+        server sent us an error and raise it if so."""
+        try:
+            return self.sock.send(data)
+        except BrokenPipeError:
+            # Check if the server responded with an error saying why it booted us:
+            response = self._recv_msg(timeout=0)
+            self.sock.close()
+            if response:
+                raise _make_exception(response) from None
+            raise
+        except ConnectionResetError:
+            self.sock.close()
+            raise
+
+    def _retry_queued(self):
+        """Retry sending as much previously queued data as we can without blocking. On
+        BrokenPipeError, check if the server sent us an error and raise it if so."""
+        try:
+            while self._sendqueue:  # while it's non-empty:
+                sent = self._checksend(self._sendqueue.peek())
+                self._sendqueue.done(sent)
+        except BlockingIOError:
+            pass
+        except ConnectionResetError:
+            self.sock.close()
+            raise
+
+    def write(self, data):
+        """Send as much as we can without blocking. If sending would block, queue unsent
+        data for later. On BrokenPipeError, check if the server sent us an error and
+        raise it if so. This method will always attempt to re-send previously-queued
+        data before attempting to send new data."""
+
+        if not isinstance(data, bytes):
+            data = data.encode('utf8')
+
+        # Retry previously-queued data:
+        self._retry_queued()
+        try:
+            # Try sending new data:
+            while data:
+                sent = self._checksend(data)
+                data = data[sent:]
+        except BlockingIOError:
+            # Queue unsent data for later
+            self._sendqueue.put(data)
 
     def close(self):
+        """Close the socket. Attempt to send all queued unsent data to the server and
+        cleanly close the connection to it, raising exceptions if anything goes wrong"""
+        if self.sock.fileno() == -1:
+            return
         try:
-            # TODO: send all unsent data
+            # Attempt to flush unsent data:
+            poller = select.epoll()
+            poller.register(self.sock, select.EPOLLOUT)
+            while self._sendqueue:
+                if poller.poll(self.timeout):
+                    self._retry_queued()
+                else:
+                    raise TimeoutError(
+                    "Timed out flushing unsent data on close(). "
+                    + f"{len(self._sendqueue)} bytes not sent."
+                )
             self.sock.shutdown(socket.SHUT_WR)
-            response = self._recv_msg(self.timeout)
+            response = self._recv_msg()
             if response != GOODBYE:
                 raise _make_exception(response)
         finally:
             self.sock.close()
+
+    def __del__(self):
+        self.close()
